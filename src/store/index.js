705a776bf5c053143d7672274327ce2bb0c0b81b
Radhikasuresh <radhikasuresh550@gmail.com>
2023-06-24 19:46:36 +0530

    redux create store replaced to legacy_createStore

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,4 @@
-import { createStore } from "redux";
+import { legacy_createStore as createStore } from "redux";
 import storage from "redux-persist/lib/storage"; // defaults to localStorage
 import { persistStore, persistReducer } from "redux-persist";
 import { composeWithDevTools } from "redux-devtools-extension";
