7734bbd15ba5ec7d622a45b8d7f90a8a87936a6d
Radhikasuresh <radhikasuresh550@gmail.com>
2023-06-29 17:28:42 +0530

    AND operator replaced to ternary

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -123,42 +123,43 @@ const Dashboard = () => {
             <div className="Queries_sq__home__cont__2LKRX ">
               {/* left */}
               <div className="Queries_sq__list__1nUJ9">
-                {tickets?.length &&
-                  tickets.map((ticket) => (
-                    <div
-                      className="Queries_sq__list__item__FVhGh"
-                      key={ticket._id}
-                    >
-                      <div className="Queries_sq__list__tile__head__169P7">
-                        <span className="Queries_sq__tile__title__357Tm">
-                          {ticket.title}
-                        </span>
-                        <div className="Queries_query__status__btn__cont__1Gdq0">
-                          <div className="Queries_query__status__btn__mKjvg Queries_query__status__unassigned__2I66S">
-                            {ticket.status}
+                {tickets?.length
+                  ? tickets.map((ticket) => (
+                      <div
+                        className="Queries_sq__list__item__FVhGh"
+                        key={ticket._id}
+                      >
+                        <div className="Queries_sq__list__tile__head__169P7">
+                          <span className="Queries_sq__tile__title__357Tm">
+                            {ticket.title}
+                          </span>
+                          <div className="Queries_query__status__btn__cont__1Gdq0">
+                            <div className="Queries_query__status__btn__mKjvg Queries_query__status__unassigned__2I66S">
+                              {ticket.status}
+                            </div>
                           </div>
+                          <button className="Buttons_nano__accept__2ILBE">
+                            <img
+                              src="https://www.zenclass.in/Icons/tick.svg"
+                              alt="toggle hide/view"
+                            />
+                          </button>
                         </div>
-                        <button className="Buttons_nano__accept__2ILBE">
-                          <img
-                            src="https://www.zenclass.in/Icons/tick.svg"
-                            alt="toggle hide/view"
-                          />
-                        </button>
+                        <div className="d-flex justify-content-between">
+                          <span className="Queries_category__tag__2Znr0">
+                            {ticket.description}
+                          </span>
+                          <span className="Queries_timestamp__1FG2g">
+                            {formatDate(ticket.createdAt)}
+                          </span>
+                        </div>
                       </div>
-                      <div className="d-flex justify-content-between">
-                        <span className="Queries_category__tag__2Znr0">
-                          {ticket.description}
-                        </span>
-                        <span className="Queries_timestamp__1FG2g">
-                          {formatDate(ticket.createdAt)}
-                        </span>
-                      </div>
-                    </div>
-                  ))}
+                    ))
+                  : ""}
               </div>
 
               {/* right */}
-              {selectedTicket && (
+              {selectedTicket ? (
                 <div className="Queries_sq__single__item__3QDru">
                   <span className="Queries_recent__title___0YjV">
                     Recent query
@@ -201,6 +202,8 @@ const Dashboard = () => {
                     </div>
                   </span>
                 </div>
+              ) : (
+                ""
               )}
             </div>
           </div>
