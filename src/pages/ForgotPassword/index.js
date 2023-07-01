5d8c4c16f3d3c5be0655116b3047a87ba568445d
Radhikasuresh <radhikasuresh550@gmail.com>
2023-07-02 03:03:13 +0530

    forgot password & reset password functionality created

--- src/pages/ForgotPassword/index.js
+++ src/pages/ForgotPassword/index.js
@@ -3,18 +3,39 @@ import * as yup from "yup";
 import api from "axiosConfig";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { useNavigate } from "react-router-dom";
 
 const schema = yup.object().shape({
   email: yup.string().email().required(),
+  password: yup
+    .string()
+    .test(
+      "isRequired",
+      "Password must be at least 8 Character",
+      function (value) {
+        if (!value) return true;
+        if (value) {
+          return value.length >= 8 && value.length <= 32;
+        }
+      }
+    ),
+  otp: yup
+    .string()
+    .test("isRequired", "OTP should be 6 characters", function (value) {
+      if (!value) return true;
+      if (value) {
+        return value.length === 6;
+      }
+    }),
 });
 
 const ForgotPassword = () => {
   const [isValidEmail, setIsValidEmail] = useState(false);
+  const navigate = useNavigate();
   const {
     register,
     handleSubmit,
     formState: { errors },
-    reset,
     setError,
   } = useForm({
     resolver: yupResolver(schema),
@@ -22,8 +43,19 @@ const ForgotPassword = () => {
 
   const onSubmitHandler = async (data) => {
     try {
-      const response = await api.post("/api/users/forgotpassword", data);
-      if (response) setIsValidEmail(true);
+      if (isValidEmail) {
+        const response = await api.post("/api/users/resetpassword", {
+          email: data.email,
+          password: data.password,
+          otp: data.otp,
+        });
+        if (response) navigate("/queries");
+      } else {
+        const response = await api.post("/api/users/forgotpassword", {
+          email: data.email,
+        });
+        if (response) setIsValidEmail(true);
+      }
     } catch (error) {
       setError("catch", error.response.data);
     }
@@ -47,10 +79,46 @@ const ForgotPassword = () => {
               name="email"
               placeholder="Eg: John@abc.com"
               type="email"
+              disabled={isValidEmail}
             />
             <p className="text-danger">{errors.email?.message}</p>
           </div>
         </div>
+        {isValidEmail ? (
+          <>
+            <div class="form-group mt-2">
+              <label for="password" class="label-style mb-0">
+                Enter New Password
+              </label>
+              <div>
+                <input
+                  {...register("password")}
+                  class="formInputs"
+                  name="password"
+                  type="password"
+                />
+                <p className="text-danger">{errors.password?.message}</p>
+              </div>
+            </div>
+            <div class="form-group mt-2">
+              <label for="otp" class="label-style mb-0">
+                Enter OTP
+              </label>
+              <div>
+                <input
+                  {...register("otp")}
+                  class="formInputs"
+                  name="otp"
+                  type="text"
+                />
+                <p className="text-danger">{errors.otp?.message}</p>
+              </div>
+            </div>
+          </>
+        ) : (
+          ""
+        )}
+
         <button
           type="submit"
           class="col-md-12 btn btn-lg btn-block login-btn mt-4 mb-4"
